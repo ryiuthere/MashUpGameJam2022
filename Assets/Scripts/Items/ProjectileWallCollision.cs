0c42249df26aacec49ee398cb2cb202b01c99b6f
Ralston Yiu <ralstonyiu@yahoo.com>
2022-11-05 14:57:56 -0500

    Fixed projectile and wall interaction

---
+++ Assets/Scripts/Items/ProjectileWallCollision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Items
+{
+    public class ProjectileWallCollision : MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
